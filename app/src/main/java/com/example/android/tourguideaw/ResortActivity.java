3d29b9c31bc817884c444ddcdabd9c7612dcc7a6
lehieu30794 <lehieu30794@gmail.com>
2018-07-24 13:21:24 -0600

    Polish visual2

--- app/src/main/java/com/example/android/tourguideaw/ResortActivity.java
+++ app/src/main/java/com/example/android/tourguideaw/ResortActivity.java
@@ -21,6 +21,37 @@ public class ResortActivity extends AppCompatActivity {
                 "Linh Ung Pagoda is situated near the station, with a cable car nearby to carry tourists to and from the resort."
                 , R.drawable.bana));
 
+        placeInfo.add(new PlaceInfo("Ba Na hills", "Bà Nà Hill Station is a hill station is a hill station and resort " +
+                "located in the Trường Sơn Mountains west of the city of Da Nang, in central Vietnam. " +
+                "The station, advertised as \"the Da Lat of Danang province\" by local tourism authorities, was founded in 1919 by French colonists. " +
+                "Linh Ung Pagoda is situated near the station, with a cable car nearby to carry tourists to and from the resort."
+                , R.drawable.bana));
+
+        placeInfo.add(new PlaceInfo("Ba Na hills", "Bà Nà Hill Station is a hill station is a hill station and resort " +
+                "located in the Trường Sơn Mountains west of the city of Da Nang, in central Vietnam. " +
+                "The station, advertised as \"the Da Lat of Danang province\" by local tourism authorities, was founded in 1919 by French colonists. " +
+                "Linh Ung Pagoda is situated near the station, with a cable car nearby to carry tourists to and from the resort."
+                , R.drawable.bana));
+
+        placeInfo.add(new PlaceInfo("Ba Na hills", "Bà Nà Hill Station is a hill station is a hill station and resort " +
+                "located in the Trường Sơn Mountains west of the city of Da Nang, in central Vietnam. " +
+                "The station, advertised as \"the Da Lat of Danang province\" by local tourism authorities, was founded in 1919 by French colonists. " +
+                "Linh Ung Pagoda is situated near the station, with a cable car nearby to carry tourists to and from the resort."
+                , R.drawable.bana));
+
+        placeInfo.add(new PlaceInfo("Ba Na hills", "Bà Nà Hill Station is a hill station is a hill station and resort " +
+                "located in the Trường Sơn Mountains west of the city of Da Nang, in central Vietnam. " +
+                "The station, advertised as \"the Da Lat of Danang province\" by local tourism authorities, was founded in 1919 by French colonists. " +
+                "Linh Ung Pagoda is situated near the station, with a cable car nearby to carry tourists to and from the resort."
+                , R.drawable.bana));
+
+        placeInfo.add(new PlaceInfo("Ba Na hills", "Bà Nà Hill Station is a hill station is a hill station and resort " +
+                "located in the Trường Sơn Mountains west of the city of Da Nang, in central Vietnam. " +
+                "The station, advertised as \"the Da Lat of Danang province\" by local tourism authorities, was founded in 1919 by French colonists. " +
+                "Linh Ung Pagoda is situated near the station, with a cable car nearby to carry tourists to and from the resort."
+                , R.drawable.bana));
+
+
         PlaceAdapter placeInfoAdapter = new PlaceAdapter(this, placeInfo);
 
         ListView resortListView = (ListView) findViewById(R.id.resort_list_view);
