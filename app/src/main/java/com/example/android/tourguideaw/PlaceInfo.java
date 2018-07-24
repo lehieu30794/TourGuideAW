bebf67bb3969a1025cdda40d3f9c57bb12a6f14e
lehieu30794 <lehieu30794@gmail.com>
2018-07-24 09:44:10 -0600

    Create a new class to handle ArrayList

---
+++ app/src/main/java/com/example/android/tourguideaw/PlaceInfo.java
@@ -0,0 +1,10 @@
+package com.example.android.tourguideaw;
+
+public class PlaceInfo {
+
+    private String mPlaceName;
+
+    private String mPlaceDetail;
+
+    public PlaceInfo(String a, String b){}
+}
