fc287382443e6c92d39d94e0736cee72e4a23f0e
lehieu30794 <lehieu30794@gmail.com>
2018-07-25 08:37:58 -0600

    Finish modifying ResortFragment

--- app/src/main/java/com/example/android/tourguideaw/ResortFragment.java
+++ app/src/main/java/com/example/android/tourguideaw/ResortFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 
 
 public class ResortFragment extends Fragment {
@@ -22,6 +24,106 @@ public class ResortFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.list_item_view,
                 container, false);
+
+
+        // COPY THE CODE FROM RESORT ACTIVITY OVER
+        ArrayList<PlaceInfo> placeInfo = new ArrayList<PlaceInfo>();
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+        placeInfo.add(new PlaceInfo("Novotel Danang Premier Han River",
+                "Uniquely located in downtown Danang, on the beautifully-designed " +
+                        "West Bank of the Han River, the Novotel has taken its place as a leading " +
+                        "hotel venue for both business and leisure travelers. It is Danang's first " +
+                        "upscale internationally-managed hotel with serviced-apartments, " +
+                        "with the highest and hippest panoramic bar in the city alongside " +
+                        "world-class facilities and restaurants. So be it business or pleasure, " +
+                        "know that at the Novotel Danang, " +
+                        "our professional staff are looking forward to welcoming you.",
+                R.drawable.novotel));
+
+        PlaceAdapter placeInfoAdapter = new PlaceAdapter(getActivity(), placeInfo);
+
+        ListView resortListView = (ListView) rootView.findViewById(R.id.resort_list_view);
+
+        resortListView.setAdapter(placeInfoAdapter);
 
         return rootView;
     }
