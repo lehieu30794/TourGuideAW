1a8caf614d0c8230efefd7d0a995cf8e8188d982
lehieu30794 <lehieu30794@gmail.com>
2018-07-25 08:33:08 -0600

    Modify LayoutInflater to display list_item_view

--- app/src/main/java/com/example/android/tourguideaw/ResortFragment.java
+++ app/src/main/java/com/example/android/tourguideaw/ResortFragment.java
@@ -20,7 +20,10 @@ public class ResortFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resort, container, false);
+        View rootView = inflater.inflate(R.layout.list_item_view,
+                container, false);
+
+        return rootView;
     }
 
 }
