951587b04e2b1ba82375d6c23cb3ebc84f245f16
lehieu30794 <lehieu30794@gmail.com>
2018-07-25 21:06:42 -0600

    Prepare to hook up CategoryAdapter (Adapter for ViewPager) and ViewPagers

---
+++ app/src/main/java/com/example/android/tourguideaw/CategoryAdapter.java
@@ -0,0 +1,28 @@
+package com.example.android.tourguideaw;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+// In order to populate pages (fragment) onto the screen, need to have an adapter, which is FragmentPagerAdapter
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            // Why we need to have a word "new" here? Why can't we just call w/o 'new'
+            return new ResortFragment();
+        } else {
+            return new TouristAttractionFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 1;
+    }
+}
